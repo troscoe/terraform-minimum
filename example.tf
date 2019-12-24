6a5ef569729a42f4b813f758fe288ae18c6a9f9a
Tristan Roscoe <troscoe@gmail.com>
2019-12-23 19:07:51 -0500

    Update example.tf

--- example.tf
+++ example.tf
@@ -3,7 +3,20 @@ provider "aws" {
   region     = "us-east-1"
 }
 
+variable "key_name" {}
+
+resource "tls_private_key" "example" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated_key" {
+  key_name   = "${var.key_name}"
+  public_key = "${tls_private_key.example.public_key_openssh}"
+}
+
 resource "aws_instance" "example" {
   ami             = "ami-00068cd7555f543d5"
   instance_type   = "t2.micro"
+  key_name      = "${aws_key_pair.generated_key.key_name}"
 }
